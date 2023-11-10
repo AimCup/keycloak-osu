069408e6e08dfd9189a94f8d34bdc30e47c06336
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-11-10 13:22:42 +0100

    Removed config properties for osu! oauth2

--- src/main/java/xyz/aimcup/keycloakosu/OsuIdentityProviderFactory.java
+++ src/main/java/xyz/aimcup/keycloakosu/OsuIdentityProviderFactory.java
@@ -4,10 +4,6 @@ import org.keycloak.broker.provider.AbstractIdentityProviderFactory;
 import org.keycloak.broker.social.SocialIdentityProviderFactory;
 import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.provider.ProviderConfigProperty;
-import org.keycloak.provider.ProviderConfigurationBuilder;
-
-import java.util.List;
 
 public class OsuIdentityProviderFactory extends AbstractIdentityProviderFactory<OsuIdentityProvider>
         implements SocialIdentityProviderFactory<OsuIdentityProvider> {
@@ -27,19 +23,6 @@ public class OsuIdentityProviderFactory extends AbstractIdentityProviderFactory<
     @Override
     public IdentityProviderModel createConfig() {
         return new OsuIdentityProviderConfig();
-    }
-
-    @Override
-    public List<ProviderConfigProperty> getConfigProperties() {
-        return ProviderConfigurationBuilder.create()
-                .property()
-                .name("avatar_url")
-                .label("Avatar URL")
-                .helpText("Avatar URL")
-                .type(ProviderConfigProperty.STRING_TYPE)
-                .defaultValue("${avatar_url}")
-                .add()
-                .build();
     }
 
     @Override
